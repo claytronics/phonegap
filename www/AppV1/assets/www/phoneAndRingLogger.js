6536de0ebc612e0937e56806cca5b7815e46189f
karthik k <karthikkamarapu32@gmail.com>
2013-08-18 19:47:46 -0400

    Getting phone&ring data

--- www/AppV1/assets/www/phoneAndRingLogger.js
+++ www/AppV1/assets/www/phoneAndRingLogger.js
@@ -9,16 +9,21 @@ function PhoneCallsLogger()
 {
 	   //alert("PHONE CALLS LOGGER");
 		phoneStateListener.start(function (state,number){
-		   //alert("STATE in phone"+state);
-		   //alert("number in phone"+number);
-          checkDataReady(2);
+		 //alert("STATE in phone"+state);
+		 //alert("number in phone"+number);
           phonenumber = number;
+          var time=getTimeStamp();
+          var ur2 = rootURI + 'record/' + uid + '/'+ '2/' + encodeURIComponent(time) + '/' + currLat + '/' + currLon + '/' + ax + '/' + ay + '/' + az + '/' + phonenumber + '/' + encodeURIComponent(time) + '/' + null + '/' + 'false';
+		  //alert(ur2);
+      	  doRequest(ur2,0);
+          phoneNumber = null;
+          //checkDataReady(2);
           });
 
-       ringStateListener.start(function (state,number){
-		  //alert("state"+state);
+       ringStateListener.start(function (states,number){
+          state=number;
+          //alert("state"+state);
           checkDataReady(4);
-          state = state;
-          //alert("state in ring"+state);
-          });
+
+         });
 }
